9535df23182e55b33af9c91c7cdb2bbcdf8f4eee
kiwiscode <ayktkav@gmail.com>
2025-03-06 18:14:04 +0100

    Healthy route moved to parent app file

--- backend/javascript/app.js
+++ backend/javascript/app.js
@@ -5,13 +5,11 @@ require("dotenv").config();
 require("./db");
 require("./config")(app);
 
-app.get("/healthy", async (req, res) => {
-  try {
-    res.json("healthy");
-  } catch (error) {
-    console.error("Internal server error!");
-    res.status(500).json({ errorMessage: "Internal server error" });
-  }
+// health check
+app.get("/", (_, res) => {
+  return res.status(200).json({
+    status: "healthy",
+  });
 });
 
 module.exports = app;

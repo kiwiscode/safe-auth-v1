f17e6a81c2658ba2a211b3eae296975524892aee
kiwiscode <ayktkav@gmail.com>
2025-03-06 18:28:07 +0100

    refactor to use CommonJS module syntax with require

--- backend/javascript/utils/bcrypt.js
+++ backend/javascript/utils/bcrypt.js
@@ -1,7 +1,11 @@
-import bcrypt from "bcrypt";
+const bcrypt = require("bcrypt");
 
-export const hashValue = async (val, saltRounds) =>
-  bcrypt.hash(val, saltRounds || 10);
+const hashValue = async (val, saltRounds) => bcrypt.hash(val, saltRounds || 10);
 
-export const compareValue = async (val, hashedValue) =>
+const compareValue = async (val, hashedValue) =>
   bcrypt.compare(val, hashedValue).catch(() => false);
+
+module.exports = {
+  hashValue,
+  compareValue,
+};

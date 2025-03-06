2738c85c5deb097d7530cce613f44ca4998a52cc
kiwiscode <ayktkav@gmail.com>
2025-03-06 23:54:50 +0100

    feat(): create verification code model

---
+++ backend/javascript/models/verification.model.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require("mongoose");
+
+const verificationCodeSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+    index: true,
+  },
+  type: { type: String, required: true },
+  createdAt: { type: Date, required: true, default: Date.now },
+  expiresAt: { type: Date, required: true },
+});
+
+const VerificationCode = model("VerificationCode", verificationCodeSchema);
+
+module.exports = VerificationCode;

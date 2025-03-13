cd9315f584c5aa40862ac8b7d3816ea617a123a8
kiwiscode <ayktkav@gmail.com>
2025-03-13 01:12:35 +0100

    feat(): add login, logout, and email verification routes

--- backend/javascript/routes/auth.route.js
+++ backend/javascript/routes/auth.route.js
@@ -1,9 +1,17 @@
 const { Router } = require("express");
-const { registerHandler } = require("../controllers/auth.controller");
+const {
+  registerHandler,
+  loginHandler,
+  logoutHandler,
+  verifyEmailHandler,
+} = require("../controllers/auth.controller");
 
 const authRoutes = Router();
 
 // prefix: /auth
 authRoutes.post("/register", registerHandler);
+authRoutes.post("/login", loginHandler);
+authRoutes.get("/logout", logoutHandler);
+authRoutes.get("/email/verify/:code", verifyEmailHandler);
 
 module.exports = authRoutes;

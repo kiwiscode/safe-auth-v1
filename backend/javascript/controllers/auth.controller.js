bd74158ef4bf062f3e8ac659d0b735b26597fcb0
kiwiscode <ayktkav@gmail.com>
2025-03-13 01:11:53 +0100

    feat(): add login, logout, and email verification handlers

--- backend/javascript/controllers/auth.controller.js
+++ backend/javascript/controllers/auth.controller.js
@@ -1,9 +1,19 @@
-const { CREATED, OK, UNAUTHORIZED } = require("../constants/http");
-const { createAccount } = require("../services/auth.service");
+const { CREATED, OK } = require("../constants/http");
+const {
+  createAccount,
+  loginUser,
+  verifyEmail,
+} = require("../services/auth.service");
 const catchErrors = require("../utils/catchErrors");
-const { setAuthCookies } = require("../utils/cookies");
+const { setAuthCookies, clearAuthCookies } = require("../utils/cookies");
+const { verifyToken } = require("../utils/jwt");
+const SessionModel = require("../models/session.model");
 
-const { registerSchema } = require("./auth.schemas");
+const {
+  registerSchema,
+  loginSchema,
+  verificationCodeSchema,
+} = require("./auth.schemas");
 
 const registerHandler = catchErrors(async (req, res) => {
   const request = registerSchema.parse({
@@ -16,6 +26,47 @@ const registerHandler = catchErrors(async (req, res) => {
     .json(user);
 });
 
+const loginHandler = catchErrors(async (req, res) => {
+  const request = loginSchema.parse({
+    ...req.body,
+    userAgent: req.headers["user-agent"],
+  });
+  const { accessToken, refreshToken } = await loginUser(request);
+
+  // set cookies
+  return setAuthCookies({ res, accessToken, refreshToken })
+    .status(OK)
+    .json({ message: "Login successful" });
+});
+
+const logoutHandler = catchErrors(async (req, res) => {
+  const accessToken = req.cookies.accessToken;
+  const { payload } = verifyToken(accessToken || "");
+
+  console.log("payload:", payload);
+
+  if (payload) {
+    // remove session from db
+    await SessionModel.findByIdAndDelete(payload.sessionId);
+  }
+
+  // clear cookies
+  return clearAuthCookies(res)
+    .status(OK)
+    .json({ message: "Logout successful" });
+});
+
+const verifyEmailHandler = catchErrors(async (req, res) => {
+  const verificationCode = verificationCodeSchema.parse(req.params.code);
+
+  await verifyEmail(verificationCode);
+
+  return res.status(OK).json({ message: "Email was successfully verified" });
+});
+
 module.exports = {
   registerHandler,
+  loginHandler,
+  logoutHandler,
+  verifyEmailHandler,
 };

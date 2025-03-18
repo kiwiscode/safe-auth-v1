dfa9f4fefd7a80927a004e956a778b8cbda62538
kiwiscode <ayktkav@gmail.com>
2025-03-18 14:10:21 +0100

    feat(): add password reset endpoint

--- backend/javascript/routes/auth.route.js
+++ backend/javascript/routes/auth.route.js
@@ -6,6 +6,7 @@ const {
   logoutHandler,
   verifyEmailHandler,
   sendPasswordResetHandler,
+  resetPasswordHandler,
 } = require("../controllers/auth.controller");
 
 const authRoutes = Router();
@@ -17,5 +18,6 @@ authRoutes.get("/refresh", refreshHandler);
 authRoutes.get("/logout", logoutHandler);
 authRoutes.get("/email/verify/:code", verifyEmailHandler);
 authRoutes.post("/password/forgot", sendPasswordResetHandler);
+authRoutes.post("/password/reset", resetPasswordHandler);
 
 module.exports = authRoutes;
